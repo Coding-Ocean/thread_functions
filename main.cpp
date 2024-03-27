226917e8ddb32126952bfa7c402bcb69cca9a900
Coding-Ocean <eijo524@gmail.com>
2024-03-27 17:42:53 +0900

    member_function

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include<thread>
 using namespace std;
 
+class Hoge {
+public:
+	void func(int n){
+		while (n-- > 0)
+			cout << n << endl;
+	}
+};
 
 int main() {
-
-
+	Hoge h;
+	thread t(&Hoge::func, &h, 10);
+	
+	//ƒ‰ƒ€ƒ_Ž®‚ðŽg‚¤‚Æ‚«
+	//thread t( [&h]{ h.func(10); } );
+	
+	t.join();
 }
