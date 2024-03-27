d3b39cf00e826ebcf55d6a0abbc8acc950111e48
Coding-Ocean <eijo524@gmail.com>
2024-03-27 17:30:30 +0900

    functor

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 #include<thread>
 using namespace std;
 
+class functor {
+public:
+	void operator()(int n) {
+		while (n-- > 0)cout << n << endl;
+	}
+};
 
 int main() {
+	//thread t( (functor()) );
+	
+	functor f;
+	thread t2(f,10);
 
-
+	//t.join();
+	t2.join();
 }
